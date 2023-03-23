d61b359ee397b83b21fc266041dc095cd55ae1d0
qa-isorn-2-bkon@example.com <qa-isorn-2-bkon@example.com>
2023-03-23 16:22:34 +0000

    First commit of promoted project

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://x-15767.stoplight-dev.com/api/v1/projects/cHJqOjE1OTM/spectral.js?branch_slug=main&token=71db977e-51b1-4985-a985-56f22b354c53";
+export default { extends: ruleset };
